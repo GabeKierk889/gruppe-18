5f0107f167d1daa903c7f41a5cb7e0d5ea11d687
Gabriel H. Kierkegaard <s215825@dtu.dk>
2021-09-28 09:14:09 +0200

    Worked on main game

--- 18_del1/18_del1/src/Game/DiceGame.java
+++ 18_del1/18_del1/src/Game/DiceGame.java
@@ -1,2 +1,50 @@
-package Game;public class DiceGame {
+package Game;
+import java.util.Scanner;
+
+public class DiceGame {
+    public static void main(String[] args) {
+        int playerOneSum = 0, playerTwoSum = 0;
+        boolean gameOn = true, playerOneTurn = true, playerTwoTurn = true;
+        String turn;
+
+        DiceCup dc = new DiceCup();
+        Scanner scanner = new Scanner(System.in);
+
+        while (gameOn) {
+
+            while (playerOneTurn) {
+                if (playerOneSum >= 40) {
+                    gameOn = false;
+                } else {
+                    System.out.print("Player 1 turn, press enter to roll the dice: ");
+                    turn = scanner.nextLine();
+                    dc.roll();
+                    System.out.println(dc);
+                    playerOneSum += dc.getSum();
+                    System.out.println("The total sum is: " + playerOneSum);
+                    System.out.println();
+                    playerOneTurn = false;
+                }
+            }
+
+            playerTwoTurn = true;
+
+            while (playerTwoTurn) {
+                if (playerTwoSum >= 40) {
+                    gameOn = false;
+                } else {
+                    System.out.print("Player 2 turn, press enter to roll the dice");
+                    turn = scanner.nextLine();
+                    dc.roll();
+                    System.out.println(dc);
+                    playerTwoSum += dc.getSum();
+                    System.out.println("The total sum is: " + playerTwoSum);
+                    System.out.println();
+                    playerTwoTurn = false;
+                }
+            }
+
+            playerOneTurn = true;
+        }
+    }
 }
