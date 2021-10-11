ef864038f3e2eb5ed407fb1440f68140d1ff3564
Xiao Chen <xchen2305@gmail.com>
2021-10-11 19:42:41 +0200

    Updated field class

--- 18_del2/src/Game/Field.java
+++ 18_del2/src/Game/Field.java
@@ -3,21 +3,28 @@ package Game;
 public class Field {
     private static int totalnumberOfFields = 0; //for Matador, remember when initializing to make an empty/placeholder field to let the actual fields start from array index 1
     private final int fieldNumber;
-    private final String fieldName; //field description for this version of the dice game
-    private int fieldEffect;
-    private boolean fieldPositiveEffect; // false for withdraw, true for deposit
+    private final String fieldDescription;
+    private final String fieldSubtext;
+    private final int fieldEffect;
+    private final boolean fieldPositiveEffect; // false for withdraw, true for deposit
 
-    public Field(String fieldName, boolean fieldPositiveEffect, int fieldEffect) {
-        this.fieldName = fieldName;
+    public Field(String fieldSubtext, String fieldDescription, boolean fieldPositiveEffect, int fieldEffect) {
+        this.fieldSubtext = fieldSubtext;
+        this.fieldDescription = fieldDescription;
         this.fieldNumber = totalnumberOfFields;
+        totalnumberOfFields++;
         this.fieldPositiveEffect = fieldPositiveEffect;
         this.fieldEffect = fieldEffect;
-        totalnumberOfFields++;
     }
-    public boolean getFieldType (){ return fieldPositiveEffect; }
+    //no setters as all attributes are final variables
+    public boolean getFieldPosiveEffect (){ return fieldPositiveEffect; }
     public int getFieldEffect (){ return fieldEffect; }
+    public int getFieldNumber (){ return fieldNumber; }
+    public int getTotalnumberOfFields (){ return totalnumberOfFields-1; }
+    public String getFieldDescription (){ return fieldDescription; }
+    public String getFieldSubtext() {return fieldSubtext; }
 
     public String toString() {
-        return "Field " + fieldNumber  + ". " + fieldName;
+        return "Field " + fieldNumber  + ". " + fieldDescription;
     }
 }
