b4734fe51ae400cc5a0fbddc2ebda954f86dc1ac
Xiao Chen <xchen2305@gmail.com>
2021-11-01 13:52:15 +0100

    Copied over test classes used in del_2

---
+++ 18_del3/src/Test/Java/AccountTest.java
@@ -0,0 +1,64 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AccountTest {
+    Account account = new Account();
+
+    // Test that checks for the balance withdrawed can never be negative, and also that the account balance never can
+    // go under zero.
+    @org.junit.jupiter.api.Test
+    void withdrawMoney() {
+        for (int i = -1000; i <= 1000; i++) {
+            if (i >= 0) {
+                account.withdrawMoney(i);
+                assertEquals(account.getBalance(), 1000 - i);
+                account.setCurrentBalance(1000);
+            }
+
+            if (i < 0) {
+                account.withdrawMoney(i);
+                assertEquals(1000,account.getBalance());
+            }
+        }
+    }
+
+    // Test that checks for the balance deposited can only be positive.
+    @org.junit.jupiter.api.Test
+    void depositMoney() {
+
+        for (int i = -1000; i<= 1000; i++) {
+            if (i >= 0) {
+                account.depositMoney(i);
+                assertEquals(account.getBalance(),1000 + i);
+                account.setCurrentBalance(1000);
+            }
+
+            if (i < 0) {
+                account.depositMoney(i);
+                assertEquals(1000,account.getBalance());
+            }
+        }
+    }
+
+    // Test that checks that it's possible to set the balance, and also that the balance can not be set to a negative
+    // number.
+    @org.junit.jupiter.api.Test
+    void setCurrentBalance() {
+        for (int i = -1000; i <= 1000; i++) {
+            if (i >= 0) {
+                account.setCurrentBalance(i);
+                assertEquals(account.getBalance(), i);
+            }
+
+            if (i < 0) {
+                account.setCurrentBalance(i);
+                assertEquals(1000, account.getBalance());
+            }
+        }
+    }
+
+    // Test that checks that it's possible to get the current balance.
+    @org.junit.jupiter.api.Test
+    void getBalance() {
+        assertEquals(account.getBalance(),account.getBalance());
+    }
+}
