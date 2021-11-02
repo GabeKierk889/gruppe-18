c1176ffbaccfbc04168659203c9352cd888a891d
Xiao Chen <xchen2305@gmail.com>
2021-11-02 14:51:37 +0100

    Updated afsender og modtager

--- Ugeopgaver/src/main/java/Afsender.java
+++ Ugeopgaver/src/main/java/Afsender.java
@@ -1,5 +1,17 @@
 public class Afsender {
     private String navn;
     private String mailadresse;
+    private Outbox outbox;
 
+    public Outbox getOutbox() {
+        return outbox;
+    }
+
+    public void setName(String name) {
+        this.navn = name;
+    }
+
+    public void newMessage() {
+
+    }
 }
--- Ugeopgaver/src/main/java/Modtager.java
+++ Ugeopgaver/src/main/java/Modtager.java
@@ -1,4 +1,9 @@
 public class Modtager {
     private String navn;
     private String mailadresse;
+    private Inbox inbox;
+
+    public Inbox getInbox() {
+        return inbox;
+    }
 }
