1a02d1e9d1b28a8e0ce4fddb7ff919e0cec74bc5
Mark Bidstrup <markbidstrup@gmail.com>
2021-11-02 14:49:29 +0100

    adding Inbox and Outbox class

---
+++ Ugeopgaver/src/main/java/Inbox.java
@@ -0,0 +1,15 @@
+public class Inbox {
+    private Besked beskeder = new Besked();
+
+    public void getMessage() {
+    }
+
+    public void open() {
+    }
+
+    public void delete() {
+    }
+
+    public void reply() {
+    }
+}
---
+++ Ugeopgaver/src/main/java/Outbox.java
@@ -0,0 +1,13 @@
+public class Outbox {
+    private Besked beskeder = new Besked();
+
+    public void open() {
+    }
+
+    public void delete() {
+    }
+
+    public void newMessage() {
+
+    }
+}
