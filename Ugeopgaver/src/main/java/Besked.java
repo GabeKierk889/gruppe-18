df8d8de2223d8c9610c056b071c9be6e3b114abe
Maj <Majkyllesbech@gmail.com>
2021-11-02 15:03:27 +0100

    have made private string and public void

---
+++ Ugeopgaver/src/main/java/Besked.java
@@ -0,0 +1,19 @@
+public class Besked {
+
+    private String Afsender;
+    private String Modtager;
+    private String Text;
+
+   public void setRecipient(){
+
+   }
+   public void addRecipient(){
+
+   }
+   public void setText(){
+
+   }
+   public void addAttachment(){
+
+   }
+}
