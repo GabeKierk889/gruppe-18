081a397e5e9feedf6c9847e237f702863ef2fdd5
Xiao Chen <xchen2305@gmail.com>
2021-11-04 19:48:07 +0100

    Created game logic methods within Game class

--- 18_del3/src/main/java/Game.java
+++ 18_del3/src/main/java/Game.java
@@ -1,18 +1,32 @@
 public class Game {
-    private Player player[];
+    public final int MAXPLAYERS = 4;
+    public final int MINPLAYERS = 2;
+    private String[] playerNames;
+    private Player[] players;
     private Die die;
     private int currentPlayer;
     private int nextPlayer;
-    private final int totalPlayers = 2; // to be updated
+    private int totalPlayers;
     private int winner;
     public final int STARTBONUS = 2;
     private Board board;
 
-    public Game() {
-        currentPlayer = 1;
-        nextPlayer = 1;
-        die = new Die();
-        board = new Board();
+    public Game(String ... player_names) {
+        playerNames = player_names;
+        if (playerNames.length > MAXPLAYERS || playerNames.length <MINPLAYERS)
+            System.out.println("Error - game cannot be started. There must be "+
+                    MINPLAYERS + "-"+ MAXPLAYERS+ " players in the game.");
+        else {
+            totalPlayers = playerNames.length;
+            players = new Player[totalPlayers];
+            for (int i = 0; i < totalPlayers; i++) {
+                players[i] = new Player(player_names[i]);
+            }
+            die = new Die();
+            board = new Board();
+            currentPlayer = 1;
+            nextPlayer = 1; // this will be changed through the nextplayer method
+        }
     }
 
     public void switchTurn(boolean extraTurn) {
@@ -34,21 +48,32 @@ public class Game {
 
     public boolean checkIsAnyoneBankrupt() {
         boolean anyoneBankrupt = false;
-        // check if any player has lost all their money
-        return anyoneBankrupt;
+        for (int i = 0; i < totalPlayers; i++) {
+            if (players[i].getAccount().getBalance() <= 0) {
+                players[i].isBankrupt(true);
+                anyoneBankrupt = true;
+            }
+        }        return anyoneBankrupt;
     }
 
     public boolean isGameOver() {
         return checkIsAnyoneBankrupt(); // game ends if a player goes bankrupt
     }
 
-    public void setWinner(int winner) {
-        this.winner = winner;
-    }
+    public void determineWinner() {
+        int mostMoneyAmount = 0;
+        int playerWithMostMoney = 0;
+        for (int i = 0; i < totalPlayers; i++) {
+            if (players[i].getAccount().getBalance() > mostMoneyAmount) {
+                mostMoneyAmount = players[i].getAccount().getBalance();
+                playerWithMostMoney = i + 1; // player number is 1 greater than player-array-position
+            }
+        }
+        winner = playerWithMostMoney;    }
 
     public Die getDie() { return die;  }
     public Board getBoard() { return board; }
-    public Player getPlayerObject(int playernumber) { return player[playernumber-1]; }
+    public Player getPlayerObject(int playernumber) { return players[playernumber-1]; }
     public int getCurrentPlayerNumber() { return currentPlayer; }
     public int getTotalPlayers() {return totalPlayers; }
     public int getNextPlayerNumber() { return nextPlayer;}
