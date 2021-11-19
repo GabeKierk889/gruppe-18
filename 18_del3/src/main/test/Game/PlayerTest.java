e513fb380d394aea8d787664d105b02f8e5f365f
Maj <Majkyllesbech@gmail.com>
2021-11-19 16:06:17 +0100

    updated account test

--- 18_del3/src/main/test/Game/PlayerTest.java
+++ 18_del3/src/main/test/Game/PlayerTest.java
@@ -5,13 +5,48 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
+    Player player = new Player("Xiao");
+    Board board = new Board();
+    Die die = new Die();
 
+    // test af alle
     @Test
     void movePlayerSteps() {
+        int playerOnField = player.OnField();
+        die.roll();
+        int steps = die.getFaceValue();
+
+        player.movePlayerSteps(steps);
+        assertEquals(playerOnField + steps,player.OnField());
+    }
+
+    @Test
+    void movePlayerStepsBoundary(){
+        int totalFields = Board.getTotalNumberOfFields();
+        player.movePlayerToField(totalFields-1);
+
+        player.movePlayerSteps(1);
+        assertEquals(0,player.OnField());
     }
 
     @Test
     void collectStartBonus() {
+        int totalFields = Board.getTotalNumberOfFields();
+        player.movePlayerToField(totalFields-1);
+
+        int balance = player.getAccount().getBalance();
+        player.movePlayerSteps(1);
+        player.collectStartBonus(1);
+        int startBonus = Account.STARTBONUS;
+        assertEquals(balance + startBonus,player.getAccount().getBalance());
+    }
+    @Test
+    void collectStartBonusNotEligibleForBonus() {
+
+        int balance = player.getAccount().getBalance();
+        player.movePlayerSteps(1);
+        player.collectStartBonus(1);
+        assertEquals(balance,player.getAccount().getBalance());
     }
 
     @Test
