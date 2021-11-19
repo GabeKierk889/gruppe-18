bc4d289d641424d1c0ce0eabe5324fbb0483b6e7
Maj <Majkyllesbech@gmail.com>
2021-11-19 14:55:42 +0100

    updated account test

---
+++ 18_del3/src/main/test/Game/PlayerTest.java
@@ -0,0 +1,64 @@
+package Game;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @Test
+    void movePlayerSteps() {
+    }
+
+    @Test
+    void collectStartBonus() {
+    }
+
+    @Test
+    void movePlayerToField() {
+    }
+
+    @Test
+    void testMovePlayerToField() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getAccount() {
+    }
+
+    @Test
+    void onField() {
+    }
+
+    @Test
+    void setIsBankrupt() {
+    }
+
+    @Test
+    void setIsInJail() {
+    }
+
+    @Test
+    void getIsInJail() {
+    }
+
+    @Test
+    void getIsBankrupt() {
+    }
+
+    @Test
+    void giveReleaseFromJailCard() {
+    }
+
+    @Test
+    void returnReleaseFromJailCard() {
+    }
+
+    @Test
+    void hasAReleaseFromJailCard() {
+    }
+}
