65524e79f021c1bdb7001f3a5d5d658a0cad17a6
MarkBidstrup <91070400+MarkBidstrup@users.noreply.github.com>
2022-01-06 11:19:10 +0100

    added GameSettings class

---
+++ 18_final/src/main/java/Models/GameSettings.java
@@ -0,0 +1,13 @@
+package Models;
+
+public class GameSettings {
+
+    public static final int MINNUMOFPLAYERS = 3;
+    public static final int MAXNUMOFPLAYERS = 6;
+    public static final int STARTINGBALANCE = 30000;
+    public static final int STARTBONUS = 4000;
+    public static final int JAILFEE = 1000;
+    public static final int STATETAXAMOUNT = 2000;
+    public static final int INCOMETAXAMOUNT = 4000;
+
+}
