25877b6ac49a282b498f97b305cefad786561cc5
MarkBidstrup <91070400+MarkBidstrup@users.noreply.github.com>
2022-01-07 11:46:36 +0100

    Started working on GameController and MatadorMain

--- 18_final/src/main/java/Controllers/MatadorMain.java
+++ 18_final/src/main/java/Controllers/MatadorMain.java
@@ -2,6 +2,12 @@ package Controllers;
 
 public class MatadorMain {
 
+    public static void main(String[] args) {
 
+        GameController gameController = new GameController();
+
+        initializeGame();
+
+    }
 
 }
