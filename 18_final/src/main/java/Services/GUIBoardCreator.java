29deb823d8f004f37bdc407976d8054a54182cd4
MarkBidstrup <91070400+MarkBidstrup@users.noreply.github.com>
2022-01-09 20:27:11 +0100

    Formatting GUIBoard, changes to some txt files and minor change in fields.csv

--- 18_final/src/main/java/Services/GUIBoardCreator.java
+++ 18_final/src/main/java/Services/GUIBoardCreator.java
@@ -10,8 +10,14 @@ public class GUIBoardCreator {
     private GUI_Field[] guiFields;
     private GUI_Street[] guiStreets;
     private Board board = GameController.getInstance().getBoard();
+    private FieldsDataReader fieldsDataReader = new FieldsDataReader("Fields.csv");
+    private int[] fieldPrice = fieldsDataReader.getFieldPriceArray();
+    private FileImporter fileImporter = new FileImporter();
+    private String[] takeTurnMessages = fileImporter.readAllLinesInFile("GameMessages_takeTurn.txt");
+    private String[] fieldDescriptions = fileImporter.readAllLinesInFile("FieldDescriptions.txt");
 
     public GUIBoardCreator(GUI_Field[] guiFields, GUI_Street[] guiStreets) {
+
         this.guiFields = guiFields;
         this.guiStreets = guiStreets;
         setUpAndFormatGUIBoard();
@@ -23,13 +29,30 @@ public class GUIBoardCreator {
             guiStreets[i].setTitle(board.getFieldObject(i).getFieldName());
             guiStreets[i].setBackGroundColor(Color.lightGray);
             guiFields[i] = guiStreets[i];
+            // if a field is ownable, set sub text to price
+            if (
+                    !(i == 0 || i == 2 || i == 7 ||
+                        i == 10 || i == 17 || i == 20 || i == 22 ||
+                        i == 30 || i == 33 || i == 36 || i == 38)
+            ) {
+                guiFields[i].setSubText(takeTurnMessages[9]+" "+fieldPrice[i]);
+            }
+            // setup chance fields
+            if (i == 2 || i == 7 || i == 17 || i == 22 || i == 33 || i == 36) {
+                guiFields[i] = new GUI_Chance();
+                guiFields[i].setDescription("");
+
+            }
+            // setup shipping fields
+            if (i == 5 || i == 15 || i == 25 || i == 35) {
+                guiFields[i] = new GUI_Shipping();
+            }
         }
 
-        colorStreets();
-        setupShippingFields();
+        formatStreets();
+        formatShippingFields();
         setupBreweryFields();
         setupSTART();
-        setupChanceFields();
         setupRefuge();
         setupJailFields();
         setupTaxFields();
@@ -39,156 +62,87 @@ public class GUIBoardCreator {
     // alt du behøver som fx pris og feltnavn findes allerede under board.getFieldObject(int).getFieldName for eks
     // eller, det kan godt være du skal bruge nogle få strenge såsom "kr. " + fieldPrice, eller det med skattefeltet
     // men det meste andet kan hentes fra felt-objekterne
-    private void colorStreets() {
+    private void formatStreets() {
         Color orange = new Color(255, 165, 0);
         Color lilla = new Color(128, 0, 128);
 
         guiStreets[1].setBackGroundColor(Color.blue);
-        guiStreets[1].setSubText("beløb");
 
         guiStreets[3].setBackGroundColor(Color.blue);
-        guiStreets[3].setSubText("beløb");
 
         guiStreets[6].setBackGroundColor(orange);
-        guiStreets[6].setSubText("beløb");
         guiStreets[8].setBackGroundColor(orange);
-        guiStreets[8].setSubText("beløb");
         guiStreets[9].setBackGroundColor(orange);
-        guiStreets[9].setSubText("beløb");
 
         guiStreets[11].setBackGroundColor(Color.green);
-        guiStreets[11].setSubText("beløb");
         guiStreets[13].setBackGroundColor(Color.green);
-        guiStreets[13].setSubText("beløb");
         guiStreets[14].setBackGroundColor(Color.green);
-        guiStreets[14].setSubText("beløb");
-
 
         guiStreets[16].setBackGroundColor(Color.gray);
-        guiStreets[16].setSubText("beløb");
         guiStreets[18].setBackGroundColor(Color.gray);
-        guiStreets[18].setSubText("beløb");
         guiStreets[19].setBackGroundColor(Color.gray);
-        guiStreets[19].setSubText("beløb");
 
         guiStreets[21].setBackGroundColor(Color.red);
-        guiStreets[21].setSubText("beløb");
         guiStreets[23].setBackGroundColor(Color.red);
-        guiStreets[23].setSubText("beløb");
         guiStreets[24].setBackGroundColor(Color.red);
-        guiStreets[24].setSubText("beløb");
 
         guiStreets[26].setBackGroundColor(Color.white);
-        guiStreets[26].setSubText("beløb");
         guiStreets[27].setBackGroundColor(Color.white);
-        guiStreets[27].setSubText("beløb");
         guiStreets[29].setBackGroundColor(Color.white);
-        guiStreets[29].setSubText("beløb");
 
         guiStreets[31].setBackGroundColor(Color.yellow);
-        guiStreets[31].setSubText("beløb");
         guiStreets[32].setBackGroundColor(Color.yellow);
-        guiStreets[32].setSubText("beløb");
         guiStreets[34].setBackGroundColor(Color.yellow);
-        guiStreets[34].setSubText("beløb");
 
         guiStreets[37].setBackGroundColor(lilla);
-        guiStreets[37].setSubText("beløb");
         guiStreets[39].setBackGroundColor(lilla);
-        guiStreets[39].setSubText("beløb");
-
-    }
-
-    private void setupShippingFields() {
-        guiFields[5] = new GUI_Shipping();
-        guiStreets[5].setSubText("beløb");
-        guiFields[15] = new GUI_Shipping();
-        guiStreets[15].setSubText("beløb");
-        guiFields[25] = new GUI_Shipping();
-        guiStreets[25].setSubText("beløb");
-        guiFields[35] = new GUI_Shipping();
-        guiStreets[35].setSubText("beløb");
     }
 
     private void setupBreweryFields() {
         guiFields[12] = new GUI_Brewery();
-        guiFields[12].setTitle("Tuborg");
-        guiFields[12].setSubText("beløb");
+        guiFields[12].setTitle(board.getFieldObject(12).getFieldName());
+        guiFields[12].setSubText(takeTurnMessages[9]+" "+fieldPrice[12]);
         guiFields[28] = new GUI_Brewery();
-        guiFields[28].setTitle("Carlsberg");
-        guiFields[28].setSubText("beløb");
+        guiFields[28].setTitle(board.getFieldObject(28).getFieldName());
+        guiFields[28].setSubText(takeTurnMessages[9]+" "+fieldPrice[28]);
+
+    }
+
+    private void formatShippingFields() {
+        guiFields[5].setSubText(takeTurnMessages[9]+" "+fieldPrice[5]);
+        guiFields[15].setSubText(takeTurnMessages[9]+" "+fieldPrice[15]);
+        guiFields[25].setSubText(takeTurnMessages[9]+" "+fieldPrice[25]);
+        guiFields[35].setSubText(takeTurnMessages[9]+" "+fieldPrice[35]);
     }
 
     private void setupSTART() {
         guiFields[0] = new GUI_Start(
                 GameController.getInstance().getBoard().getFieldObject(0).getFieldName(),
-                "start",
-                "passér start tekst",
+                board.getFieldObject(0).getFieldName(),
+                "",
                 Color.MAGENTA,
                 Color.WHITE);
     }
 
-    private void setupChanceFields() {
-        guiFields[2] = new GUI_Chance(
-                "?",
-                "Prøv lykken",
-                "Prøv lykken",
-                Color.black,
-                Color.green
-        );
-        guiFields[7] = new GUI_Chance(
-                "?",
-                "Prøv lykken",
-                "Prøv lykken",
-                Color.black,
-                Color.green
-        );
-        guiFields[17] = new GUI_Chance(
-                "?",
-                "Prøv lykken",
-                "Prøv lykken",
-                Color.black,
-                Color.green
-        );
-        guiFields[22] = new GUI_Chance(
-                "?",
-                "Prøv lykken",
-                "Prøv lykken",
-                Color.black,
-                Color.green
-        );
-        guiFields[33] = new GUI_Chance(
-                "?",
-                "Prøv lykken",
-                "Prøv lykken",
-                Color.black,
-                Color.green
-        );
-        guiFields[36] = new GUI_Chance(
-                "?",
-                "Prøv lykken",
-                "Prøv lykken",
-                Color.black,
-                Color.green
-        );
-    }
-
     private void setupRefuge() {
         guiFields[20] = new GUI_Refuge();
+        guiFields[20].setTitle(GameController.getInstance().getBoard().getFieldObject(20).getFieldName());
+        guiFields[20].setSubText(fieldDescriptions[3]);
+        guiFields[20].setDescription(fieldDescriptions[4]);
     }
 
     private void setupTaxFields() {
         guiFields[4] = new GUI_Tax(
-                "Indkomstskat",
+                GameController.getInstance().getBoard().getFieldObject(4).getFieldName(),
                 "",
-                "Betal inkomstskat: 10% eller kr. 4000",
+                fieldDescriptions[1],
                 Color.lightGray,
                 Color.black
         );
         guiFields[38] = new GUI_Tax(
-                "Statsskat",
+                GameController.getInstance().getBoard().getFieldObject(38).getFieldName(),
                 "",
-                "Ekstraordinær statsskat: Betal kr. 2000",
+                fieldDescriptions[2],
                 Color.lightGray,
                 Color.black
         );
@@ -196,10 +150,10 @@ public class GUIBoardCreator {
 
     private void setupJailFields() {
         guiFields[10] = new GUI_Jail();
-        guiFields[10].setSubText("På besoeg");
+        guiFields[10].setSubText(GameController.getInstance().getBoard().getFieldObject(10).getFieldName());
 
         guiFields[30] = new GUI_Jail();
-        guiFields[30].setSubText("De fængsles");
+        guiFields[30].setSubText(GameController.getInstance().getBoard().getFieldObject(30).getFieldName());
     }
 
     public GUI_Field[] createGuiFields() {
