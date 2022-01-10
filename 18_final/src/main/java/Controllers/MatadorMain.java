fd9876addebd8b6578d7521cf58bbbf56a095a8f
Gabriel H. kierkegaard <s215825@dtu.dk>
2022-01-10 11:41:15 +0100

    Tried working on Board.buildHouse method

--- 18_final/src/main/java/Controllers/MatadorMain.java
+++ 18_final/src/main/java/Controllers/MatadorMain.java
@@ -1,16 +1,21 @@
 package Controllers;
 
+import Models.Board;
+
 public class MatadorMain {
 
     public static void main(String[] args) {
 
-        GameController gameController = GameController.getInstance();
-
-        gameController.initializeGame();
-
-        gameController.gameLoop();
+//        GameController gameController = GameController.getInstance();
+//
+//        gameController.initializeGame();
+//
+//        gameController.gameLoop();
 
 //        gameController.testMethod();
+
+        Board board = new Board();
+        board.buildHouse();
     }
 
 }
