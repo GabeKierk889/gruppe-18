ed79d551b3c9f77f419926c64c46dd715e60e8b4
DenizIsikli <s215818@dtu.dk>
2022-01-10 17:25:36 +0100

    Added MoveXFieldsBackwards card

---
+++ 18_final/src/main/java/Models/ChanceCardSubType/MoveXFieldsBackwards.java
@@ -0,0 +1,27 @@
+package Models.ChanceCardSubType;
+
+import Controllers.GameController;
+import Controllers.ViewController;
+import Models.ChanceCard;
+import Models.Player;
+
+public class MoveXFieldsBackwards extends ChanceCard {
+    private final int STEPSTOMOVE;
+
+    public MoveXFieldsBackwards(String text, int[] numOfFields) {
+        super(text);
+        STEPSTOMOVE = numOfFields[0];
+    }
+
+    @Override
+    public void effect(Player currentplayerobject) {
+        int oldLocation = currentplayerobject.OnField();
+        int newLocation = oldLocation - 3;
+        int currentPlayer = GameController.getInstance().getCurrentPlayerNum();
+
+        if (newLocation < 0) {
+            newLocation = newLocation + GameController.getInstance().getBoard().getTotalNumOfFields();
+        }
+        ViewController.getInstance().moveGUICar(oldLocation, newLocation, currentPlayer);
+    }
+}
